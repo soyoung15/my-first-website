b294a754c01f536a01fcab5a9d46a150dd6b980e
soyoung15 <sy20061115@gmail.com>
2026-05-06 02:59:06 +1000

    adding js for homepage

--- scripts.js
+++ scripts.js
@@ -1,9 +1,25 @@
 const introPage = document.getElementById("intro_page");
+const introText = document.getElementById("intro_text");
+const instructionToStart = document.getElementById("Instruction_tap");
 const actualHomepage = document.getElementById("actual_homepage");
+
+const hamburgerMenubar = document.getElementById("menu_bar");
+const menubarClose = document.getElementById("close_menu_bar");
+const mobileNavi = document.getElementById("mobile_navi");
+
+let clicked = false;
 
 actualHomepage.style.display = "none";
 
 introPage.addEventListener("click", function () {
     introPage.style.display = "none";
     actualHomepage.style.display = "block";
+});
+
+hamburgerMenubar.addEventListener("click", function () {
+    mobileNavi.classList.add("open");
+});
+
+menubarClose.addEventListener("click", function () {
+    mobileNavi.classList.remove("open");
 });
