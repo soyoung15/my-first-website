736ec4794380a63d22a47f51ee74fc23baeec945
soyoung15 <sy20061115@gmail.com>
2026-05-06 04:31:10 +1000

    fixing intropage

--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
 const introPage = document.getElementById("intro_page");
 const introText = document.getElementById("intro_text");
-const instructionToStart = document.getElementById("Instruction_tap");
 const actualHomepage = document.getElementById("actual_homepage");
+const introLogo = document.querySelector(".introLogo");
 
 const hamburgerMenubar = document.getElementById("menu_bar");
 const menubarClose = document.getElementById("close_menu_bar");
 const mobileNavi = document.getElementById("mobile_navi");
 
-let clicked = false;
-
 actualHomepage.style.display = "none";
 
+let tap = 0;
+
 introPage.addEventListener("click", function () {
-    introPage.style.display = "none";
-    actualHomepage.style.display = "block";
+    tap++;
+
+    if (tap === 1) {
+        introLogo.classList.add("moveup");
+        introText.classList.add("show");
+    } else {
+        introPage.style.display = "none";
+        actualHomepage.style.display = "block";
+    }
 });
 
 hamburgerMenubar.addEventListener("click", function () {
     mobileNavi.classList.add("open");
-});
-
-menubarClose.addEventListener("click", function () {
-    mobileNavi.classList.remove("open");
 });
